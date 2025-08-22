236605717efdb027f43d6bb63899951129be3314
boolean-solo <thisisprithwi@gmail.com>
2025-08-22 22:41:23 +0600

    2nd sprint

--- client/src/pages/SearchHome.jsx
+++ client/src/pages/SearchHome.jsx
@@ -189,8 +189,58 @@ const [users, setUsers] = useState([]);
             )}
           </div>
 
+          {/* RIGHT */}
+          <div className="hidden w-1/4 h-full lg:flex flex-col gap-8 overflow-y-auto">
+            {/* FRIEND REEQUESTS */}
+            <div className="w-full bg-primary shadow-sm rounded-lg px-6 py-5">
+              <div className="flex items-center justify-between text-xl text-ascent-1 pb-2 border-b border-[#66666645]">
+                <span> Friend Request</span>
+                <span>{friendRequest?.length}</span>
+              </div>
+              <div className="w-full flex flex-col gap-4 pt-4">
+                {friendRequest?.map(({ _id, requestFrom: from }, index) => (
+                  <div
+                    className="flex items-center justify-between"
+                    key={index + _id}
+                  >
+                    <Link
+                      to={"/profile/" + from._id}
+                      key={from?._id}
+                      className="w-full flex gap-4 items-center cursor-pointer"
+                    >
+                      <img
+                        src={from?.profileUrl ?? NoProfile}
+                        alt={from?.firstName}
+                        className="w-10 h-10 object-cover rounded-full"
+                      />
+                      <div className="flex-1 ">
+                        <p className="text-base font-medium text-ascent-1">
+                          {from?.firstName} {from?.lastName}
+                        </p>
+                        <span className="text-sm text-ascent-2">
+                          {from?.profession ?? "No Profession"}
+                        </span>
+                      </div>
+                    </Link>
 
-        
+                    <div className="flex gap-1">
+                      <CustomButton
+                        onClick={() => acceptFriendRequest(_id, "Accepted")}
+                        title="Accept"
+                        containerStyles="bg-[#0444a4] text-xs text-white px-1.5 py-1 rounded-full"
+                      />
+                      <CustomButton
+                        title="Deny"
+                        onClick={() => acceptFriendRequest(_id, "Denied")}
+                        containerStyles="border border-[#666] text-xs text-ascent-1 px-1.5 py-1 rounded-full"
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
 
